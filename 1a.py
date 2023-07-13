2140bd4d29e6768111e5e7978e25d91100dbf0a4
Ohk Soe Htet <ohksoe2002@gmail.com>
2023-07-13 13:50:58 +0800

    chosing first fighter and more logic added

--- 1a.py
+++ 1a.py
@@ -1,5 +1,5 @@
 import random
-#test
+
 # texts
 ACTION_TEXT = "Actions: \n1. Attack \n2. Defend \n3. Use skill \n4. Quit/Restart"
 # creating characters
@@ -30,7 +30,7 @@ class Character:
         self.hp -= damage - self.dp
 
     def defend(self):
-        self.dp += 8
+        self.dp += 8 #this should only last for 1 round
 
 class PlayerCharacter(Character):
     def __init__(self,jobClass):
@@ -46,13 +46,13 @@ def test():
     print("skill used")
 
 # main game flow
-not_finished = True
 print("You can choose two characters to fight from these: archer,wizard,knight and werewolf")
 
 choice1 = input("choose your first character to fight: ")
 player1 = PlayerCharacter(choice1)
 choice2 = input("choose another one: ")
 player2 = PlayerCharacter(choice2)
+player_team = [player1,player2]
 
 enemy_team = list(set(CHARACTERS) - {choice1, choice2})
 enemy1 = Character(enemy_team[0])
@@ -60,20 +60,34 @@ enemy2 = Character(enemy_team[1])
 
 # chosing characters
 print(f"You chose {player1.jobClass} and {player2.jobClass}. The enemy characters are {enemy1.jobClass} and {enemy2.jobClass}.")
-print("Choose your first character to fight")
+first_fighter = input(f"You will fight with the {enemy1.jobClass} first, choose your first character to fight, 1/2 : ")
+if first_fighter == "2":
+    player_team.reverse()
+
+not_finished = True
 
 while not_finished:
+    fighter = player_team[0]
+    enemy_fighter = enemy_team[0]
     #options
     print(ACTION_TEXT)
     action = input("Action: ")
 
     if action == "1":
-        player1.attack(enemy1)
-        print(enemy1.hp)
+        # 1.attack(2) and 2.attack(2) # both attacks, depend on the sp
+        print("attacked")
     elif action == "2":
-        player1.defend()
-        print(player1.dp)
+        # 1.defend() and 2.attack(2) # player defend, dont know if it should depends on sp
+        print("defended") 
     elif action == "3":
-        player1.useSkill(test)
-    else:
-        break
+        fighter.useSkill(test)
+    if action == "4":
+        # will ask whether user will quit or restart
+        not_finished = False
+
+    # if fighter or enemy fighter hp drops to 0, removes it from the list
+
+    if len(player_team) == 0:
+        print("You lose")
+    elif len(enemy_team) == 0:
+        print("You win")
